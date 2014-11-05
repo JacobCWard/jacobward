09706d6e781aae509c75c06ed5c0b10086296a6e
jacobward1898 <jacobward1898@gmail.com>
2014-11-04 20:29:10 -0700

    nov01-d
    1-4

--- week01/1-4.c
+++ week01/1-4.c
@@ -1,1 +1,19 @@
-//4.	Write code that uses a for loop and prints out a list of the squares of n from n=1 to n=100, giving n and n^2  (ex. 3 -> 9).
+//4.	Write code that uses a for loop and prints out a list of the squares of
+//n from n=1 to n=100, giving n and n^2  (ex. 3 -> 9).
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    for(n=1;n<=100;n++){
+        if(n%2==1){
+            printf("n= %d\tn^2= %d\t",n,n*n);
+            if(n*n<100){
+                printf("\t");
+            }
+        }
+        else{
+            printf("n= %d\tn^2= %d\n",n,n*n);
+        }
+    }
+}
