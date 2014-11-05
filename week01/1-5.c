0dc96e5f422a43ae633d32e441e5b9c6dd090ee3
jacobward1898 <jacobward1898@gmail.com>
2014-11-04 21:46:44 -0700

    NOV04-2146

--- week01/1-5.c
+++ week01/1-5.c
@@ -1,1 +1,26 @@
-//5.	Write a function that prints out if an integer is even or odd.  Test it on 4 different integers.
+//5.	Write a function that prints out if an integer is even or odd. Test it on 4 different integers.
+
+#include <stdio.h>
+
+int main(){
+    int a[4];
+    int i;
+    printf("Enter Integer 0: ");
+    scanf("%d",&a[0]);
+    printf("Enter Integer 1: ");
+    scanf("%d",&a[1]);
+    printf("Enter Integer 2: ");
+    scanf("%d",&a[2]);
+    printf("Enter Integer 3: ");
+    scanf("%d",&a[3]);
+    printf("Integer:\n0\t1\t2\t3\n%d\t%d\t%d\t%d\n",a[0],a[1],a[2],a[3]);
+    for(i=0;i<4;i++){
+        if(a[i]%2==1){
+            printf("Odd\t");
+        }
+        else{
+            printf("Even\t");
+        }
+    }
+    printf("\n");
+}
--- week01/1-6.c
+++ week01/1-6.c
@@ -1,1 +1,21 @@
 //6.	Write and test a function that computes the distance between two points in 2D.
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int pt1[2];
+    int pt2[2];
+    double d;
+
+    printf("Enter x1: ");
+    scanf("%d",&pt1[0]);
+    printf("Enter y1: ");
+    scanf("%d",&pt1[1]);
+    printf("Enter x2: ");
+    scanf("%d",&pt2[0]);
+    printf("Enter y2: ");
+    scanf("%d",&pt2[1]);
+    d = sqrt(pow(pt2[0]-pt1[0],2)+pow(pt2[1]-pt1[1],2));
+    printf("Distance between pt1 and pt2 = %f\n",d);
+}
